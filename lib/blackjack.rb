cfc4874ca0ccdcc618d1c38e018806f45ccc2745
generous-program-0200 <harrycopley@vultuslimited.com>
2018-12-07 13:52:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,25 +1,32 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  1 + rand(11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(cardnum)
+  puts "Your cards add up to #{cardnum}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+  end
+
+def get_user_input(answer)
+  answer = gets.chomp
+  if answer = "h"
+    puts
+  elsif answer = "s"
+    puts 
+  elsif
+    answer = "exit"
+  end
 end
 
-def get_user_input
-  # code #get_user_input here
-end
-
-def end_game
-  # code #end_game here
+def end_game(num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
